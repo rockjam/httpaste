c508c7f049efff31fc5f9548b4f98823dd61f236
rockjam <5min4eq.unity@gmail.com>
2017-06-15 02:07:30 +0300

    temporary use runtime based interpolators

--- interpolators/src/main/scala/com/github/rockjam/happypaste/curl/package.scala
+++ interpolators/src/main/scala/com/github/rockjam/happypaste/curl/package.scala
@@ -1,14 +1,20 @@
 package com.github.rockjam.happypaste
 
-import com.github.rockjam.happypaste.macroimpl.ParserMacro
 import com.github.rockjam.happypaste.model.HttpRequestBlueprint
-
-import scala.language.experimental.macros
+import fastparse.core.Parsed
 
 package object curl {
 
+//  implicit class CurlStringContext(val ctx: StringContext) extends AnyVal {
+//    def curl(): HttpRequestBlueprint = macro ParserMacro.curlImpl
+//  }
+
   implicit class CurlStringContext(val ctx: StringContext) extends AnyVal {
-    def curl(): HttpRequestBlueprint = macro ParserMacro.curlImpl
+    def curl(): HttpRequestBlueprint =
+      CurlParser.commandParser.parse(ctx.parts.mkString("")) match {
+        case Parsed.Success(result, _) => result
+        case Parsed.Failure(_, _, _)   => throw new RuntimeException("Failed to parse curl request")
+      }
   }
 
 }
--- interpolators/src/main/scala/com/github/rockjam/happypaste/http/package.scala
+++ interpolators/src/main/scala/com/github/rockjam/happypaste/http/package.scala
@@ -1,14 +1,21 @@
 package com.github.rockjam.happypaste
 
-import com.github.rockjam.happypaste.macroimpl.ParserMacro
 import com.github.rockjam.happypaste.model.HttpRequestBlueprint
-
-import scala.language.experimental.macros
+import fastparse.core.Parsed
 
 package object http {
 
+//  implicit class RequestMessageStringContext(val ctx: StringContext) extends AnyVal {
+//    def http(): HttpRequestBlueprint = macro ParserMacro.httpImpl
+//  }
+
   implicit class RequestMessageStringContext(val ctx: StringContext) extends AnyVal {
-    def http(): HttpRequestBlueprint = macro ParserMacro.httpImpl
+    def http(): HttpRequestBlueprint =
+      RequestMessageParser.requestMessage.parse(ctx.parts.mkString("")) match {
+        case Parsed.Success(result, _) => result
+        case Parsed.Failure(_, _, _) =>
+          throw new RuntimeException("Failed to parse request message")
+      }
   }
 
 }
